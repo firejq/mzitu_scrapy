ffa6a6a682bb798f12f79ae49d59f1a440a33f01
firejq <firejq@outlook.com>
2017-09-07 01:50:06 +0800

    fix the middlewares

--- mzitu_scrapy/settings.py
+++ mzitu_scrapy/settings.py
@@ -22,22 +22,22 @@ NEWSPIDER_MODULE = 'mzitu_scrapy.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See
 # http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
@@ -55,10 +55,13 @@ ROBOTSTXT_OBEY = False
 # See
 # http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    'mzitu_scrapy.middlewares.RefererMiddleware': 543,
-    'mzitu_scrapy.middlewares.RandomUserAgentMiddleware': 542,  # TODO how to set this value
-    # 'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
-    # 'mzitu_scrapy.middlewares.ProxyMiddleware': 100,
+    # 'scrapy.downloadermiddlewares.defaultheaders.DefaultHeadersMiddleware':
+    #     None,
+    # 'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': None,
+    # 'mzitu_scrapy.middlewares.ProxyMiddleware': 751,
+    'mzitu_scrapy.middlewares.RefererMiddleware': 752,
+    'mzitu_scrapy.middlewares.RandomUserAgentMiddleware': 753,
+
 }
 
 # Enable or disable extensions
@@ -71,7 +74,6 @@ DOWNLOADER_MIDDLEWARES = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'mzitu_scrapy.pipelines.MzituScrapyPipeline': 300,
-    # 'scrapy.contrib.pipeline.images.ImagesPipeline': 300
 }
 
 IMAGES_STORE = 'D:\\news_mzitu'
